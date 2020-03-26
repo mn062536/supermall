e5c8779491147efca791cdd81628dd3d72f72cc5
wangchao <532424917@qq.com>
2020-03-26 18:41:11 +0800

    提交tabbar

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   configureWebpack: {
+//     resolve:{
+//       extensions:[],
+//       alias:{
+//         'assets':'@/assets',
+//         'components':'@/components',
+//         'network':'@/network',
+//         'views':'@/views',
+//       }
+//     }
+//   }
+// }
